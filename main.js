0edcc73173c815054173db917e8bbc86c1ed44ca
Diego Mondaca <diegomondaca6a@gmail.com>
2022-11-01 00:26:31 -0300

    input

---
+++ main.js
@@ -0,0 +1,5 @@
+alert("INPUT/OUTPUT")
+var mascota1 =prompt("Nombre de la Mascota 1")
+var mascota2 =prompt("Nombre de la Mascota 2")
+
+console.log("Quiero mucho a mis mascotas",mascota1,"y",mascota2)
